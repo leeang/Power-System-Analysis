6ca8fe9a4564344e1ef5c4f7e11bcc40f87cb057
Ang Li <angleedomain@gmail.com>
2016-04-18 15:59:33 +1000

    new e1_q2.m

---
+++ ws2/e1_q2.m
@@ -0,0 +1,22 @@
+clear;
+
+lambda = 4;
+BETA = 0;
+P = 0.1;
+
+index = 1;
+while P*lambda <= 0.98
+	P_vector(index) = P * lambda;
+	V(:, index) = solve_eq3(P_vector(index), BETA);
+
+    lambda = lambda + 0.1;
+    index = index + 1;
+end
+
+P_vector(index) = 0.9801999;
+V(:, index) = solve_eq3(P_vector(index), 0);
+
+plot(P_vector, V);
+xlabel('P (pu)');
+ylabel('V (pu)');
+title('P-V curve');
