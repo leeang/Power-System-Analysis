3619f19dbec52e18c66ec9f5968bf22abb025efc
Ang Li <angleedomain@gmail.com>
2016-04-18 15:10:58 +1000

    new solve_eq3.m

---
+++ ws2/solve_eq3.m
@@ -0,0 +1,14 @@
+function V_2 = solve_eq3(P_D, beta)
+	V_1 = 1;
+	Z_line = 0.01 + 0.5j;
+
+	Y_line = 1/Z_line;
+	G_line = real(Y_line);
+	B_line = imag(Y_line);
+	Y_line = abs(Y_line);
+
+	syms V_2 positive;
+	eqn = V_2^4 * Y_line^2 + V_2^2 * (2 * P_D * (G_line - beta * B_line) - Y_line^2 * V_1^2) - P_D^2 * (1+beta^2) == 0;
+	V_2 = solve(eqn, 'Real', true);
+	V_2 = double(V_2);
+end
